f64c0e994c137d138093440051c419a8a73eb33e
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-05-27 12:34:28 +0530

    data ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,14 @@
+import sys
+import os
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from pymongo import MongoClient
 from tj_mongo import MongoUrl
 import pandas as pd
-import os
+from dataclasses import dataclass
+
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
@@ -24,12 +31,46 @@ def get_data():
    
    df = pd.DataFrame.from_records(data).drop('_id',axis=1)
    return df
-   
+
+@dataclass
+class DataIngestionconfig:
+   train_data_path:str = os.path.join('artifacts' , 'train.csv')
+   test_data_path:str = os.path.join('artifacts' , 'test.csv')
+   raw_data_path:str = os.path.join('artifacts','raw.csv')
+
+### create the data ingestion class
+class DataIngestion:
+   def __init__(self) -> None:
+      self.ingestion_config = DataIngestionconfig()
+
+   def initiate_data_ingestion(self):
+      logging.info('Data Ingestion method starts')
+      try:
+         df = pd.read_csv(os.path.join('notebooks/data','breast_cancer.csv'))
+         logging.info("Dataset read as pandas Dataframe")
+
+         os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+         df.to_csv(self.ingestion_config.raw_data_path,index=False)
+
+         logging.info('Raw data is created')
+
+         train_set , test_set = train_test_split(df,test_size=0.3 , random_state=42)
+         train_set.to_csv(self.ingestion_config.train_data_path,index =False,header = True)
+         test_set.to_csv(self.ingestion_config.test_data_path,index =False,header = True)
+         logging.info('Ingestion of data is completed')
+
+         return (
+            self.ingestion_config.train_data_path,
+            self.ingestion_config.test_data_path
+         )
+
+      except Exception as e:
+         logging.info('Exception occured at data ingestion stage')
+         raise CustomException(e,sys)
+      
 
 if __name__ == "__main__":   
   
   data = get_data()
-  os.chdir('../')
-  path = os.path.dirname(os.getcwd())
   file_name = 'breast_cancer.csv'
-  data.to_csv(os.path.join(path,'notebooks',file_name),index=False)
+  data.to_csv(os.path.join('notebooks/data',file_name),index=False)
